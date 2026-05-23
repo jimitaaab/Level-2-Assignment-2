0932138e0f6ee63fe27da5ab1716ac61407132ae
jimitaaab <jawadulim2003@gmail.com>
2026-05-23 16:12:03 +0600

    4. Get All Issues

--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -5,7 +5,7 @@ import auth from '../../middleware/auth';
 const route = Router()
 
 route.post("/issues",auth("contributor", "maintainer"),issuesController.createIssues)
-
+route.get("/issues",issuesController.getAllIssues)
 
 
 export const issuesRoute = route;
